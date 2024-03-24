2a645104e0d54da2a10f43838baeeb6aa3375e70
sehee020512 <163626147+sehee020512@users.noreply.github.com>
2024-03-24 21:02:07 +0900

    Update week1.js

--- week1-javascript/javascript-racingcar-6-main/src/week1.js
+++ week1-javascript/javascript-racingcar-6-main/src/week1.js
@@ -1,11 +1,11 @@
 const inputCars = prompt("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-inputCars = inputCars.split(",");
+const carName = inputCars.split(",");
 
 // 객체 배열
 const cars = [];
-for (let i = 0; i < inputCars.length; i++) {
+for (let i = 0; i < carName.length; i++) {
     cars.push({
-        name : carNames[i],
+        name : carName[i],
         distance : 0
     });
 }
@@ -13,6 +13,8 @@ for (let i = 0; i < inputCars.length; i++) {
 const inputNum = prompt("시도할 횟수는 몇 회인가요?");
 const num = parseInt(inputNum);
 let ranNum;
+
+console.log("실행결과");
 
 for (let n = 0; n < num ; n++){
     for (let car of cars){
@@ -23,3 +25,16 @@ for (let n = 0; n < num ; n++){
         console.log(`${car.name} : ${"-".repeat(car.distance)}`);
     }
 }
+
+let winners = [];
+let maxDis = 0;
+
+for (let car of cars){
+    if (car.distance > maxDis) maxDis = car.distance;
+}
+
+for (let car of cars){
+    if (car.distance == maxDis) winners.push(car.name);
+}
+
+console.log("최종우승자 : " + winners.join(','));
